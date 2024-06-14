50ec0881bfba6bc6c14a15af36a142b5b18cffb6
Eduarda Patricio <eduarda.santos.patricio@gmail.com>
2024-06-14 09:46:43 -0300

    Update script-selects.sql

--- relacional/script-selects.sql
+++ relacional/script-selects.sql
@@ -6,7 +6,7 @@ JOIN produto p ON a.id_produto = p.id
 GROUP BY p.nome, p.preco ORDER BY p.preco DESC;
 
 -- Media das avaliações e dos preços por categoria
-SELECT c.nome AS Categoria, ROUND(AVG(a.nota), 1) AS Media_Avaliacoes, ROUND(AVG(p.preco), 1) AS Media_Precos
+SELECT c.nome AS Categoria, ROUND(AVG(a.nota), 1) AS Media_Avaliacoes, ROUND(AVG(p.preco), 2) AS Media_Precos
 FROM avaliacao a
 JOIN produto p ON p.id = a.id_produto
 JOIN categoria c ON c. id = p.id_categoria
